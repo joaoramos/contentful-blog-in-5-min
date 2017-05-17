a8cdf4b9aff0e68f48fc78618e9db94fc014cfff
stefan judis <stefanjudis@gmail.com>
2017-05-17 09:53:58 +0200

    Adjust description meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ const config = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Create a Contentful blog in 5 minutes' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
